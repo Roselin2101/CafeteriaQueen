85df9f497f82db5146f6f9de6b6f8f87b203d880
acer <rverdufiguera@gmail.com>
2020-11-29 03:35:46 -0300

    configurar  firebase

---
+++ src/Firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app"
+import "firebase/firestore"
+
+ const firebaseConfig = {
+    apiKey: "AIzaSyBodNMQGyKHvsOPKJU0SZJqhTmLafZlQz8",
+    authDomain: "proyecto-bq.firebaseapp.com",
+    databaseURL: "https://proyecto-bq.firebaseio.com",
+    projectId: "proyecto-bq",
+    storageBucket: "proyecto-bq.appspot.com",
+    messagingSenderId: "683557347317",
+    appId: "1:683557347317:web:29baf6f99814461f7d4abc"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export {firebase}
