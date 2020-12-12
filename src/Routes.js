c587c4b97e861417a1cb2d466248360e8b98b1e0
acer <rverdufiguera@gmail.com>
2020-12-12 15:29:09 -0300

    Ultimos cambios en mesonero

--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,18 @@
 
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Home } from "./modulos/Home";
-import { Cocinero } from "./modulos/Cocinero";
-import { Mesonero } from "./modulos/Mesonero";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import { Home } from "./modulos/Home";
+// import { Cocinero } from "./modulos/Cocinero";
+// import { Mesonero } from "./modulos/Mesonero";
 
- const Routes = () => {
-  return (
-    <Router>
-    <Switch>
-      <Route path="/cocinero" component={Cocinero}/>
-      <Route path="/mesonero" component={Mesonero} />
-      <Route path="/" componente={Home}/>
-    </Switch>
-    </Router>
-  );
-};
-export default Routes;
+//  const Routes = () => {
+//   return (
+//     <Router>
+//     <Switch>
+//       <Route path="/cocinero" component={Cocinero}/>
+//       <Route path="/mesonero" component={Mesonero} />
+//       <Route path="/" componente={Home}/>
+//     </Switch>
+//     </Router>
+//   );
+// };
+// export default Routes;
